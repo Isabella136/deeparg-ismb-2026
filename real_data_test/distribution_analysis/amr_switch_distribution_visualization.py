7fa2dcfebb489a6a3fc700a25fdececd3ede1690
Nathalie Bonin <nbonin@umd.edu>
2025-10-03 10:57:52 -0400

    Finished amr switch distribution fig

--- real_data_test/distribution_analysis/amr_switch_distribution_visualization.py
+++ real_data_test/distribution_analysis/amr_switch_distribution_visualization.py
@@ -3,6 +3,17 @@ import seaborn as sn
 import pandas as pd
 import numpy as np
 from io import StringIO
+from matplotlib.colors import LinearSegmentedColormap
+
+plt.rcParams["mathtext.fontset"] = "dejavusans"
+plt.rcParams["font.family"] = "DejaVu Sans"
+
+abbrev_file = open("amr_abbrev.csv", "r")
+amr_abbrev = dict()
+for line in abbrev_file.readlines():
+    row = line.strip().split(",")
+    amr_abbrev.update({row[0]: row[1]})
+abbrev_file.close()
 
 
 distribution_file = open("amr_switch_distribution.txt", "r")
@@ -13,108 +24,143 @@ for line_num in range(33):
     line = distribution_file.readline()
     if line_num == 0 :
         continue;
-    amr_class = line.split("\t")[0]
+    amr_class = amr_abbrev[line.split("\t")[0]]
     count = int(line.split("\t")[1])
     class_count.update({amr_class:count})
 
 # Read rest of data into DataFrame
 df: pd.DataFrame = pd.read_csv(StringIO(distribution_file.read()), sep='\t', header=0)
+distribution_file.close()
 
-# Sort DataFrame 
-df.sort_values(
-    by=["Model", "Alignment Identity", "Sample", "Diamond AMR", "DeepARG AMR"], 
-    inplace=True,
-    ignore_index=True)
+# Rename AMR columns based on abbrev
+for row in range(df.shape[0]):
+    df.at[row, "Diamond AMR"] = amr_abbrev[df.at[row, "Diamond AMR"]]
+    df.at[row, "DeepARG AMR"] = amr_abbrev[df.at[row, "DeepARG AMR"]]
 
-# Make heatmap multilevel x-axis indices
-indices = pd.MultiIndex.from_arrays(
-    [df.get(["Model", "Alignment Identity", "Sample"]).drop_duplicates()["Model"].values,
-     df.get(["Model", "Alignment Identity", "Sample"]).drop_duplicates()["Alignment Identity"].values,
-     df.get(["Model", "Alignment Identity", "Sample"]).drop_duplicates()["Sample"].values],
-    names=["Model", "Alignment Identity", "Sample"])
+# Create first figure where we group by Diamond AMR, then second figure where we group by DeepARG AMR
+for (group, indiv) in [("Diamond AMR", "DeepARG AMR"), ("DeepARG AMR", "Diamond AMR")]:
+    # Sort DataFrame 
+    df.sort_values(
+        by=["Model", "Alignment Identity", "Sample", group, indiv], 
+        inplace=True,
+        ignore_index=True)
 
-# Make heatmap multilevel y-axis headers
-headers = pd.MultiIndex.from_arrays(
-    [df.get(["Diamond AMR", "DeepARG AMR"]).drop_duplicates().sort_values(
-        by=["Diamond AMR", "DeepARG AMR"], key=lambda x: x.str.lower())["Diamond AMR"].values,
-     df.get(["Diamond AMR", "DeepARG AMR"]).drop_duplicates().sort_values(
-        by=["Diamond AMR", "DeepARG AMR"], key=lambda x: x.str.lower())["DeepARG AMR"].values],
-    names=["Diamond AMR", "DeepARG AMR"])
+    # Make heatmap multilevel x-axis indices
+    indices = pd.MultiIndex.from_arrays(
+        [df.get(["Model", "Alignment Identity", "Sample"]).drop_duplicates()["Model"].values,
+        df.get(["Model", "Alignment Identity", "Sample"]).drop_duplicates()["Alignment Identity"].values,
+        df.get(["Model", "Alignment Identity", "Sample"]).drop_duplicates()["Sample"].values],
+        names=["Model", "Alignment Identity", "Sample"])
 
-print(headers)
+    # Make heatmap multilevel y-axis headers
+    headers = pd.MultiIndex.from_arrays(
+        [df.get(["Diamond AMR", "DeepARG AMR"]).drop_duplicates().sort_values(
+            by=[group, indiv], key=lambda x: x.str.lower())[group].values,
+        df.get(["Diamond AMR", "DeepARG AMR"]).drop_duplicates().sort_values(
+            by=[group, indiv], key=lambda x: x.str.lower())[indiv].values],
+        names=[group, indiv])
 
-heatmap_df = pd.DataFrame(
-    np.empty(
-        shape=[45, 97]), 
-    index=indices, columns=headers)
+    heatmap_df = pd.DataFrame(
+        np.empty(
+            shape=[45, 97]), 
+        index=indices, columns=headers)
 
-mask_df = pd.DataFrame(
-    np.full(
-        shape=[45, 97], fill_value=True), 
-    index=indices, columns=headers)
+    mask_df = pd.DataFrame(
+        np.full(
+            shape=[45, 97], fill_value=True), 
+        index=indices, columns=headers)
 
-max_val = 0
-min_val = 0
+    max_val = 0
+    min_val = 0
 
-for row in df.iterrows() :
-    model = row[1]["Model"]
-    ident = row[1]["Alignment Identity"]
-    sampl = row[1]["Sample"]
-    diamo = row[1]["Diamond AMR"]
-    deepa = row[1]["DeepARG AMR"]
+    for row in df.iterrows() :
+        model = row[1]["Model"]
+        ident = row[1]["Alignment Identity"]
+        sampl = row[1]["Sample"]
+        diamo = row[1]["Diamond AMR"]
+        deepa = row[1]["DeepARG AMR"]
+        group_curr = row[1][group]
+        indiv_curr = row[1][indiv]
 
-    heatmap_df.at[(model, ident, sampl), (diamo, deepa)] = np.log(
-        (1 + row[1]["Count"])/(
-            1 + (class_count[deepa] / (sum(class_count.values()) - class_count[diamo]) * sum(
-                df[df["Model"] == model][df["Alignment Identity"] == ident][df["Sample"] == sampl][df["Diamond AMR"] == diamo]["Count"].values))))
-    mask_df.at[(model, ident, sampl), (diamo, deepa)] = False
-    max_val = max(max_val, heatmap_df.at[(model, ident, sampl), (diamo, deepa)])
-    min_val = min(min_val, heatmap_df.at[(model, ident, sampl), (diamo, deepa)])
+        heatmap_df.at[(model, ident, sampl), (group_curr, indiv_curr)] = np.log(
+            (1 + row[1]["Count"])/(
+                1 + (class_count[deepa] / (sum(class_count.values()) - class_count[diamo]) * sum(
+                    df.loc[df["Model"] == model].loc[df["Alignment Identity"] == ident].loc[df["Sample"] == sampl].loc[df["Diamond AMR"] == diamo]["Count"].values))))
+        mask_df.at[(model, ident, sampl), (group_curr, indiv_curr)] = False
+        max_val = max(max_val, heatmap_df.at[(model, ident, sampl), (group_curr, indiv_curr)])
+        min_val = min(min_val, heatmap_df.at[(model, ident, sampl), (group_curr, indiv_curr)])
 
-from matplotlib.colors import LinearSegmentedColormap
+    # Create a custom color palette
+    custom_colors = [
+    '#D20A2E', # first color
+    '#d9d9d9', # middle color
+    '#0F52BA' # last color
+    ]
+    custom_cmap = LinearSegmentedColormap.from_list("custom_gradient", custom_colors)
+    custom_cmap.set_bad()
 
-# Create a custom color palette to go with my theme(TM)
-custom_colors = [
-   '#109368', # first color
-   '#d9d9d9', # middle color
-   '#DD7719' # last color
-]
-custom_cmap = LinearSegmentedColormap.from_list("custom_gradient", custom_colors)
-custom_cmap.set_bad()
+    # Create heatmap and colormap key
+    fig = plt.figure.Figure(figsize=[32, 15])
+    ax = fig.add_axes((0.05, 0.05, 0.8, 0.9))
+    cbar = fig.add_axes((0.94, 0.2, 0.02, 0.6))
+    heatmap = sn.heatmap(
+        heatmap_df, mask=mask_df, cmap=custom_cmap, center=0, vmax=max_val, vmin=min_val, 
+        square=True, ax=ax, cbar_ax=cbar,
+        xticklabels=heatmap_df.columns.get_level_values(indiv),
+        yticklabels=heatmap_df.index.get_level_values("Sample"))
+    cbar.tick_params(labelsize=16)
+    
+    # Set x-ticks direction and axis label depending on current grouped category
+    if group == "Diamond AMR":
+        ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation_mode="anchor", rotation=45, ha='right')
+        ax.set_xlabel(indiv, fontsize=20, color="#ffaa5b", weight="bold", style="italic")
+    else:
+        ax.xaxis.tick_top()
+        ax.xaxis.set_label_position('top') 
+        ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation_mode="anchor", rotation=45, ha='left')
+        ax.set_xlabel(r'$\underline{\textbf{\textsf{\Huge{Diamond\ AMR}}}}$', color="#3dac87", usetex=True)
 
-fig = plt.figure.Figure(figsize=[30, 15])
-ax = fig.add_axes((0.1, 0.15, 0.8, 0.7))
-cbar = fig.add_axes((0.94, 0.2, 0.02, 0.6))
+    # Change y-axis location and label
+    ax.yaxis.tick_right()
+    ax.set_yticks(ax.get_yticks(), ax.get_yticklabels(), rotation_mode="anchor", rotation=0)
+    ax.yaxis.set_label_position('right') 
+    ax.set_ylabel("Sample", fontsize=20)
 
-sn.heatmap(
-    heatmap_df, mask=mask_df, cmap=custom_cmap, center=0, vmax=max_val, vmin=min_val, 
-    square=True, ax=ax, cbar_ax=cbar,
-    xticklabels=heatmap_df.columns.get_level_values("DeepARG AMR"),
-    yticklabels=heatmap_df.index.get_level_values("Sample"))
+    # Seperate by group
+    y_groups = heatmap_df.index.to_frame(index=False).groupby(["Model", "Alignment Identity"]).size()
+    x_groups = heatmap_df.columns.to_frame(index=False).groupby([group], sort=False).size()
 
-ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation_mode="anchor", rotation=45, ha='right')
+    y_boundaries = np.cumsum(y_groups)
+    x_boundaries = np.cumsum(x_groups)
 
-# Seperate by group
-y_groups = heatmap_df.index.to_frame(index=False).groupby(["Model", "Alignment Identity"]).size()
-x_groups = heatmap_df.columns.to_frame(index=False).groupby(["Diamond AMR"], sort=False).size()
+    for y_loc in y_boundaries[:-1]:
+        ax.hlines(y_loc, xmin=ax.get_xlim()[0], xmax=ax.get_xlim()[1], colors="white", linewidth=5)
+    for x_loc in x_boundaries[:-1]:
+        ax.vlines(x_loc, ymin=ax.get_ylim()[0], ymax=ax.get_ylim()[1], colors="white", linewidth=5)
 
-y_boundaries = np.cumsum(y_groups)
-x_boundaries = np.cumsum(x_groups)
+    # Add group tick labels
+    prev_y_loc = 0
+    for y_loc, y_group in zip(y_boundaries, y_groups.index):
+        ax.text(-0.5, (y_loc+prev_y_loc)/2.0, y_group, ha="right")
+        prev_y_loc = y_loc
 
-for y_loc in y_boundaries[:-1]:
-    ax.hlines(y_loc, xmin=ax.get_xlim()[0], xmax=ax.get_xlim()[1], colors="white", linewidth=5)
-for x_loc in x_boundaries[:-1]:
-    ax.vlines(x_loc, ymin=ax.get_ylim()[0], ymax=ax.get_ylim()[1], colors="white", linewidth=5)
+    prev_x_loc = 0
+    for x_loc, x_group in zip(x_boundaries, x_groups.index):
+        if group == "Diamond AMR":
+            ax.text((x_loc+prev_x_loc)/2.0, -0.5, x_group, rotation=45)
+        else:
+            ax.text((x_loc+prev_x_loc)/2.0, y_boundaries.iloc[-1] + 0.5, x_group, rotation=45, rotation_mode="anchor", ha="right")
+        prev_x_loc = x_loc
 
-# Add group labels
-prev_y_loc = 0
-for y_loc, y_group in zip(y_boundaries, y_groups.index):
-    ax.text(x_boundaries.iloc[-1] + 0.5, (y_loc+prev_y_loc)/2.0, y_group, va="center")
-    prev_y_loc = y_loc
+    # Add group axis labels
+    ax.text(-5, y_boundaries.iloc[-1]/2.0, "(Model, Alignment Identity)", rotation=90, va="center", fontsize=20)
+    if group == "Diamond AMR":
+        ax.text(
+            x_boundaries.iloc[-1]/2.0, -2, r'$\underline{\textbf{\textsf{\Huge{Diamond\ AMR}}}}$', 
+            rotation=0, ha="center", color="#3dac87", usetex=True)
+    else:
+        ax.text(
+            x_boundaries.iloc[-1]/2.0, y_boundaries.iloc[-1] + 3, group, 
+            rotation=0, ha="center", fontsize=20, color="#ffaa5b", weight="bold", style="italic")
 
-prev_x_loc = 0
-for x_loc, x_group in zip(x_boundaries, x_groups.index):
-    ax.text((x_loc+prev_x_loc)/2.0, -0.5, x_group, rotation=45)
-    prev_x_loc = x_loc
-
-fig.savefig("test.png")
+    fig.savefig("{a}_amr_switch_distribution.png".format(a = group.split(' ')[0].lower()))
